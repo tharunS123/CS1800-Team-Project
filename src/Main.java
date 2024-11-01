e272ffc144205728c7c957da9b30524e020f41f1
Tharun <tharunsenthilkumar308@gmail.com>
2024-11-01 14:13:19 -0400

    moved things from main.java to TestDriver.java

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,7 @@
 package src;
 
-import src.PostDatabase.Post;
-import src.PostDatabase.PostDatabase;
-
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        PostDatabase postdatabase = new PostDatabase("postdatabase.txt");
-
-        createPostPrompt(postdatabase);
-
-        loadAndDisplayPosts(postdatabase);
-    }
-
-    private static void loadAndDisplayPosts(PostDatabase postdatabase) throws Exception {
-        ArrayList<Post> posts = postdatabase.getPosts();
-
-        if (posts.isEmpty()) {
-            System.out.println("No posts found.");
-        } else {
-            System.out.println("Loaded Post from the database: ");
-
-            // display all the post  in the database
-            postdatabase.readPostDatabase("postdatabase.txt");
-        }
-    }
-
-    private static void createPostPrompt(PostDatabase postdatabase) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the title of the post: ");
-        String title = scanner.nextLine();
-
-        System.out.println("Enter the content of the post: ");
-        String content = scanner.nextLine();
-
-        System.out.println("Enter the author of the post: ");
-        String author = scanner.nextLine();
-
-        String date = new Timestamp(System.currentTimeMillis()).toString();
-
-        Post post = new Post(title, content, author);
-        postdatabase.addPost(post);
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
---
+++ src/PostDatabase/TestDriver.java
@@ -0,0 +1,49 @@
+package src.PostDatabase;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TestDriver {
+    public TestDriver() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        PostDatabase postdatabase = new PostDatabase("postdatabase.txt");
+
+        createPostPrompt(postdatabase);
+
+        loadAndDisplayPosts(postdatabase);
+    }
+
+    private static void loadAndDisplayPosts(PostDatabase postdatabase) throws Exception {
+        ArrayList<Post> posts = postdatabase.getPosts();
+
+        if (posts.isEmpty()) {
+            System.out.println("No posts found.");
+        } else {
+            System.out.println("Loaded Post from the database: ");
+
+            // display all the post  in the database
+            postdatabase.readPostDatabase("postdatabase.txt");
+        }
+    }
+
+    private static void createPostPrompt(PostDatabase postdatabase) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the title of the post: ");
+        String title = scanner.nextLine();
+
+        System.out.println("Enter the content of the post: ");
+        String content = scanner.nextLine();
+
+        System.out.println("Enter the author of the post: ");
+        String author = scanner.nextLine();
+
+        String date = new Timestamp(System.currentTimeMillis()).toString();
+
+        Post post = new Post(title, content, author);
+        postdatabase.addPost(post);
+    }
+}
