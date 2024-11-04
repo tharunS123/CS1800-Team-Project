7b22b8950f2fb9d881dfc80305a0d0ff9dc6885c
Tharun <tharunsenthilkumar308@gmail.com>
2024-11-03 23:18:39 -0500

    added javadoc for Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 package src;
 
+/**
+ * The Main class serves as the entry point for the application.
+ * It contains the main method which is executed when the program runs.
+ *
+ * @version Nov 3, 2024
+ * @author Tharun Kumar and Mateo Toro Felipe
+ */
 public class Main {
+    /**
+     * The main method which serves as the entry point for the application.
+     *
+     * @param args command-line arguments passed to the program
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
